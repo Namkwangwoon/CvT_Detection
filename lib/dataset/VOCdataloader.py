ad9cf425e58f9de08587ecd116b64d822fa8bcb5
Namkwangwoon <nkw096@naver.com>
2022-06-24 02:11:00 +0900

    VOC padding removal

--- lib/dataset/VOCdataloader.py
+++ lib/dataset/VOCdataloader.py
@@ -115,8 +115,9 @@ class VOCDataset(Dataset):
         nw, nh = int(scale * w), int(scale * h)
         image_resized = cv2.resize(image, (nw, nh))
 
-        pad_w = _pad - nw % _pad
-        pad_h = _pad - nh % _pad
+        # pad_w = _pad - nw % _pad
+        # pad_h = _pad - nh % _pad
+        pad_w, pad_h = 0, 0
 
         image_paded = np.zeros(shape=[nh + pad_h, nw + pad_w, 3], dtype=np.uint8)
         image_paded[:nh, :nw, :] = image_resized
